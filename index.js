3c505840f357e6039fd76c83490cf3f4e1f90bec
Majidshah83 <shahmajid508@gmail.com>
2022-03-28 18:18:13 +0500

    Data Type in javascript

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-console.log("helo majid");
+// console.log("helo majid");
 
-// / Values and Variables in JS
+// // / Values and Variables in JS
+// var myname = "majid shah";
+// var myAge = 28;
+// var _myage = 23;
+// var _my__name = "ali shah";
+// var $myname = "ali";
+// console.log(_myage);
+// console.log(myAge);
+// console.log(myname);
+// console.log(_my__name);
+// console.log($myname);
+
+//Data type in javascript JS
+//There six data type in javascript
+
+var $_myage = 23;
 var myname = "majid shah";
-var myAge = 28;
-var _myage = 23;
-var _my__name = "ali shah";
-var $myname = "ali";
-console.log(_myage);
-console.log(myAge);
-console.log(myname);
-console.log(_my__name);
-console.log($myname);
+var iammajid = true;
+var iammid = false;
+
+console.log(typeof myname);
+console.log(typeof iammajid);
+console.log(typeof iammid);
+console.log(23 + 24);
+console.log("java" + "script");
+console.log(true + true);
+console.log(false + true);
