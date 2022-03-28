aa2241e1d2b49603e5a15bfcedfe7f18c66c0c89
Majidshah83 <shahmajid508@gmail.com>
2022-03-28 17:51:12 +0500

    Varibable define

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("helo majid");
+
+// / Values and Variables in JS
+var myname = "majid shah";
+var myAge = 28;
+var _myage = 23;
+var _my__name = "ali shah";
+var $myname = "ali";
+console.log(_myage);
+console.log(myAge);
+console.log(myname);
+console.log(_my__name);
+console.log($myname);
