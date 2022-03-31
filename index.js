85fba4af14278559f561711d20b44ee5512fea62
Majidshah83 <shahmajid508@gmail.com>
2022-03-31 14:41:42 +0500

    increment and decrment number

--- index.js
+++ index.js
@@ -15,15 +15,38 @@
 //Data type in javascript JS
 //There six data type in javascript
 
-var $_myage = 23;
-var myname = "majid shah";
-var iammajid = true;
-var iammid = false;
+// var $_myage = 23;
+// var myname = "majid shah";
+// var iammajid = true;
+// var iammid = false;
 
-console.log(typeof myname);
-console.log(typeof iammajid);
-console.log(typeof iammid);
-console.log(23 + 24);
-console.log("java" + "script");
-console.log(true + true);
-console.log(false + true);
+// console.log(typeof myname);
+// console.log(typeof iammajid);
+// console.log(typeof iammid);
+// console.log(23 + 24);
+// console.log("java" + "script");
+// console.log(true + true);
+// console.log(false + true);
+// console.log("majid");
+// alert("helo ali");
+
+//variable
+var smooth = "helo majid shah";
+console.log(smooth);
+var smooth = "458";
+console.log(smooth);
+document.getElementById("header1");
+document.getElementsByClassName("headr2");
+// var age = prompt("what is your age??");
+// document.getElementById("p2").innerHTML = age;
+
+//number
+var p1 = 10;
+console.log(p1);
+//increment and decrement
+var p1 = 10;
+p1--;
+p1++;
+console.log(p1);
+//function
+
