0a54a1342acdd8c57f02b4618d862945b9abdd10
Majidshah83 <shahmajid508@gmail.com>
2022-03-31 18:35:47 +0500

    Strings (Common Methods)

---
+++ string.js
@@ -0,0 +1,11 @@
+//String in javascript (common methods)
+let fruit = "apple";
+let fruits = "apple\nmango"; //next line
+console.log(fruits);
+console.log(fruit.length);
+console.log(fruit.indexOf("a"));
+console.log(fruit.slice(0, 2));
+console.log(fruit.replace("app", "232"));
+console.log(fruit.toLocaleUpperCase());
+console.log(fruit.toLocaleLowerCase());
+console.log(fruit.split(''));
