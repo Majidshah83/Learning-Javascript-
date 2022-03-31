fd16501661c9f53f40d8016a3c66a2df0788d28c
Majidshah83 <shahmajid508@gmail.com>
2022-03-31 17:57:39 +0500

    while loop and for loop

---
+++ loop.js
@@ -0,0 +1,12 @@
+//while loop
+var num = 0;
+while (num < 100) {
+  num += 1;
+  console.log(num);
+}
+
+// alert("loop");
+//for looop
+for (let num = 0; num <= 100; num++) {
+  console.log(num);
+}
