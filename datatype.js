1a9d20497d25f00c9964f8eda2bd6ee5db873fb9
Majidshah83 <shahmajid508@gmail.com>
2022-03-31 18:09:35 +0500

    Datatype javascript

---
+++ datatype.js
@@ -0,0 +1,8 @@
+//data type
+let yourage = 18; //integer
+let yourName = "ali"; //string
+let name = { frist: "ali", last: "shah" }; //object
+let truth = false; //boolen
+let groceries = ["apple", "banana", "oranges"]; //array
+let random; //undefined
+let nothing = null; //null value
